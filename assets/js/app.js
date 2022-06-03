0b0cb8435c834ba0836cbb9c8a3dabc44f5780f1
Kakouuu <cyrikchenpro@gmail.com>
2022-06-03 15:43:53 +0200

    Update portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,15 +36,14 @@ $(function() {
 		})
 		.fail(function(data) {
 			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
+			$(formMessages).addClass('success');
+			$('#name').val('');
+			$('#email').val('');
+			$('#subject').val('');
+			$('#message').val('');
 			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+			$(formMessages).text('Merci de m\'avoir contacter .');
+
 		});
 
 	});
